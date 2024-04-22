80bba57e9b90a7047b1a720b5641b62d20a32aff
Ed Elliott <goeddieuk@gmail.com>
2024-04-22 20:52:38 +0100

    wip

--- src/test/Spark.Connect.Dotnet.Tests/AssemblyInfo.cs
+++ src/test/Spark.Connect.Dotnet.Tests/AssemblyInfo.cs
@@ -1,1 +1,1 @@
-[assembly:Xunit.TestFramework("XunitCustomFrameworkTests.CustomTestFramework", "Spark.Connect.Dotnet.Tests")]
+// [assembly:Xunit.TestFramework("XunitCustomFrameworkTests.CustomTestFramework", "Spark.Connect.Dotnet.Tests")]
