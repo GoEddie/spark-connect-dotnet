429f28cc1f355ab9af5793fc8bc8d809bd9090d8
Ed Elliott <goeddieuk@gmail.com>
2024-05-16 20:27:48 +0100

    tests hang forever without the custom framework in gh actions

---
+++ src/test/Spark.Connect.Dotnet.Tests/CustomTestFramework.cs
@@ -0,0 +1,176 @@
+using System.Reflection;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace XunitCustomFrameworkTests;
+
+public class CustomTestFramework : XunitTestFramework
+{
+    public CustomTestFramework(IMessageSink messageSink)
+        : base(messageSink)
+    {
+        messageSink.OnMessage(new DiagnosticMessage("Using CustomTestFramework"));
+    }
+
+    protected override ITestFrameworkExecutor CreateExecutor(AssemblyName assemblyName)
+        => new CustomExecutor(assemblyName, SourceInformationProvider, DiagnosticMessageSink);
+
+    private class CustomExecutor : XunitTestFrameworkExecutor
+    {
+        public CustomExecutor(AssemblyName assemblyName, ISourceInformationProvider sourceInformationProvider, IMessageSink diagnosticMessageSink)
+            : base(assemblyName, sourceInformationProvider, diagnosticMessageSink)
+        {
+        }
+
+        protected override async void RunTestCases(IEnumerable<IXunitTestCase> testCases, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
+        {
+            using var assemblyRunner = new CustomAssemblyRunner(TestAssembly, testCases, DiagnosticMessageSink, executionMessageSink, executionOptions);
+            await assemblyRunner.RunAsync();
+        }
+    }
+
+    private class CustomAssemblyRunner : XunitTestAssemblyRunner
+    {
+        public CustomAssemblyRunner(ITestAssembly testAssembly, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
+            : base(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions)
+        {
+        }
+
+        protected override Task<RunSummary> RunTestCollectionAsync(IMessageBus messageBus, ITestCollection testCollection, IEnumerable<IXunitTestCase> testCases, CancellationTokenSource cancellationTokenSource)
+            => new CustomTestCollectionRunner(testCollection, testCases, DiagnosticMessageSink, messageBus, TestCaseOrderer, new ExceptionAggregator(Aggregator), cancellationTokenSource).RunAsync();
+    }
+
+    private class CustomTestCollectionRunner : XunitTestCollectionRunner
+    {
+        public CustomTestCollectionRunner(ITestCollection testCollection, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageBus messageBus, ITestCaseOrderer testCaseOrderer, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource)
+            : base(testCollection, testCases, diagnosticMessageSink, messageBus, testCaseOrderer, aggregator, cancellationTokenSource)
+        {
+        }
+
+        protected override Task<RunSummary> RunTestClassAsync(ITestClass testClass, IReflectionTypeInfo @class, IEnumerable<IXunitTestCase> testCases)
+            => new CustomTestClassRunner(testClass, @class, testCases, DiagnosticMessageSink, MessageBus, TestCaseOrderer, new ExceptionAggregator(Aggregator), CancellationTokenSource, CollectionFixtureMappings)
+                .RunAsync();
+    }
+
+    private class CustomTestClassRunner : XunitTestClassRunner
+    {
+        public CustomTestClassRunner(ITestClass testClass, IReflectionTypeInfo @class, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageBus messageBus, ITestCaseOrderer testCaseOrderer, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource, IDictionary<Type, object> collectionFixtureMappings)
+            : base(testClass, @class, testCases, diagnosticMessageSink, messageBus, testCaseOrderer, aggregator, cancellationTokenSource, collectionFixtureMappings)
+        {
+        }
+
+        protected override Task<RunSummary> RunTestMethodAsync(ITestMethod testMethod, IReflectionMethodInfo method, IEnumerable<IXunitTestCase> testCases, object[] constructorArguments)
+            => new CustomTestMethodRunner(testMethod, this.Class, method, testCases, this.DiagnosticMessageSink, this.MessageBus, new ExceptionAggregator(this.Aggregator), this.CancellationTokenSource, constructorArguments)
+                .RunAsync();
+    }
+
+    private class CustomTestMethodRunner : XunitTestMethodRunner
+    {
+        private readonly IMessageSink _diagnosticMessageSink;
+
+        public CustomTestMethodRunner(ITestMethod testMethod, IReflectionTypeInfo @class, IReflectionMethodInfo method, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageBus messageBus, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource, object[] constructorArguments)
+            : base(testMethod, @class, method, testCases, diagnosticMessageSink, messageBus, aggregator, cancellationTokenSource, constructorArguments)
+        {
+            _diagnosticMessageSink = diagnosticMessageSink;
+        }
+
+        protected override async Task<RunSummary> RunTestCaseAsync(IXunitTestCase testCase)
+        {
+            var parameters = string.Empty;
+
+            if (testCase.TestMethodArguments != null)
+            {
+                parameters = string.Join(", ", testCase.TestMethodArguments.Select(a => a?.ToString() ?? "null"));
+            }
+
+            var test = $"{TestMethod.TestClass.Class.Name}.{TestMethod.Method.Name}({parameters})";
+
+            _diagnosticMessageSink.OnMessage(new DiagnosticMessage($"STARTED: {test}"));
+
+            try
+            {
+                var task = base.RunTestCaseAsync(testCase);
+                var taskCompleted = Task.WaitAny(new []{task}, TimeSpan.FromSeconds(900000));
+                //var result = await base.RunTestCaseAsync(testCase);
+                if (-1 == taskCompleted)
+                {
+                    throw new TimeoutException($"Test {test} timed out");
+                }
+
+                var result = task.Result;
+
+                var status = result.Failed > 0 
+                    ? "FAILURE" 
+                    : (result.Skipped > 0 ? "SKIPPED" : "SUCCESS");
+
+                _diagnosticMessageSink.OnMessage(new DiagnosticMessage($"{status}: {test} ({result.Time}s)"));
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _diagnosticMessageSink.OnMessage(new DiagnosticMessage($"ERROR: {test} ({ex.Message})"));
+                throw;
+            }
+        }
+    }
+}
+
+class CustomTestMethodRunner : XunitTestMethodRunner
+{
+    private readonly IMessageSink _diagnosticMessageSink;
+
+    // We need to pass all the injected values into the base constructor
+    public CustomTestMethodRunner(ITestMethod testMethod, IReflectionTypeInfo @class, IReflectionMethodInfo method, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageBus messageBus, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource, object[] constructorArguments)
+        : base(testMethod, @class, method, testCases, diagnosticMessageSink, messageBus, aggregator, cancellationTokenSource, constructorArguments)
+    {
+        _diagnosticMessageSink = diagnosticMessageSink;
+    }
+
+    protected override async Task<RunSummary> RunTestCaseAsync(IXunitTestCase testCase)
+    {
+        // Create a text representation of the test parameters (for theory tests)
+        var parameters = string.Empty;
+
+        if (testCase.TestMethodArguments != null)
+        {
+            parameters = string.Join(", ", testCase.TestMethodArguments.Select(a => a?.ToString() ?? "null"));
+        }
+
+        // Build the full name of the test (class + method + parameters)
+        var test = $"{TestMethod.TestClass.Class.Name}.{TestMethod.Method.Name}({parameters})";
+
+        // Write a log to the output that we're starting the test
+        _diagnosticMessageSink.OnMessage(new DiagnosticMessage($"STARTED: {test}"));
+
+        try
+        {
+            // Execute the test and get the result
+            var task = base.RunTestCaseAsync(testCase);
+            var taskCompleted = Task.WaitAny(new []{task}, TimeSpan.FromSeconds(10));
+            //var result = await base.RunTestCaseAsync(testCase);
+            if (-1 == taskCompleted)
+            {
+                throw new TimeoutException($"Test {test} timed out");
+            }
+
+            var result = task.Result;
+
+            // Work out the final status of the test
+            var status = result.Failed > 0 
+                ? "FAILURE" 
+                : (result.Skipped > 0 ? "SKIPPED" : "SUCCESS");
+
+            // Write the result of the test to the output
+            _diagnosticMessageSink.OnMessage(new DiagnosticMessage($"{status}: {test} ({result.Time}s)"));
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            // Something went wrong trying to execute the test
+            _diagnosticMessageSink.OnMessage(new DiagnosticMessage($"ERROR: {test} ({ex.Message})"));
+            throw;
+        }
+    }
+}
