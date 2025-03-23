0d00225446b1647c1234a257f743970dca4fc93b
Ed Elliott <GOEddieUK@gmail.com>
2025-03-23 09:12:24 +0000

    Sample showing mixing and matching DataFrame API and Expressions / Relations

---
+++ src/example/mixing_dataframe_and_expressions/Program.cs
@@ -0,0 +1,33 @@
+﻿using Spark.Connect;
+using Spark.Connect.Dotnet.Sql;
+
+var spark = SparkSession
+    .Builder
+    .Remote("http://localhost:15002")
+    .GetOrCreate();
+    
+    var dataFrame = spark.Range(100);
+    
+    var newColumnExpression = new Expression.Types.Alias()
+    {
+        Expr = new Expression()
+        {
+            Literal = new Expression.Types.Literal()
+            {
+                String = "This is a string from .NET"
+            }
+        },
+        Name = {"NewColumnFromExpression"}
+    };
+    
+    var withColumnRelation = new Relation()
+    {
+        WithColumns = new WithColumns()
+        {
+            Input = dataFrame.Relation,
+            Aliases = {  newColumnExpression }
+        }
+    };
+    
+    var newDataFrame = new DataFrame(dataFrame.SparkSession, withColumnRelation);
+    newDataFrame.Show();
