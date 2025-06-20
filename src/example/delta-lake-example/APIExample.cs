4c9495e73b76cb00ae992cc7c8b10e9c9c9d61a4
Ed Elliott <GOEddieUK@gmail.com>
2025-06-20 18:09:45 +0100

    delta lake api

---
+++ src/example/delta-lake-example/APIExample.cs
@@ -0,0 +1,85 @@
+using System.Runtime.CompilerServices;
+using Spark.Connect;
+using Spark.Connect.Dotnet.DeltaLake;
+using Spark.Connect.Dotnet.Sql;
+using Spark.Connect.Dotnet.Sql.Types;
+using static Spark.Connect.Dotnet.Sql.Functions;
+
+namespace delta_lake_example;
+
+public class APIExample
+{
+    public void Run(SparkSession spark, string deltaPath, string csvPath)
+    {
+        Console.WriteLine($"Is the path '{csvPath} as deltaTable? {DeltaTable.IsDeltaTable(spark, csvPath)}");
+        Console.WriteLine($"Is the path '{deltaPath} as deltaTable? {DeltaTable.IsDeltaTable(spark, csvPath)}");
+        
+        var deltaTable = DeltaTable.ForPath(spark, deltaPath);
+        deltaTable.History().Show(10, 1000);
+
+        deltaTable.Update("id < 10", (Col("id"), Lit(0)));
+        
+        deltaTable.ToDF().Show(20);
+
+        var source = spark.Range( 5).WithColumn("name", Lit("teddy")).Alias("src");
+        deltaTable
+            .As("tgt")
+            .Merge(source, "src.id = tgt.id")
+            .WithSchemaEvolution()
+            .WhenNotMatchedInsertAll()
+            .WhenMatchedUpdateAll()
+            .Execute(spark);
+        
+        deltaTable.ToDF().Show(50);
+
+        deltaTable.RestoreToVersion(1);
+        
+        deltaTable.ToDF().Show(50);
+        deltaTable.Delete(Lit(true));
+        deltaTable.ToDF().Show(50);
+        
+        deltaTable.Detail().Show();
+
+        var newDeltaTable = DeltaTable
+            .CreateOrReplace(spark)
+            .TableName("deltatablefuntest")
+            .AddColumn(
+                new DeltaTableColumnBuilder("col_a")
+                    .DataType("int")
+                    .Build()
+            )
+            .AddColumn(
+                new DeltaTableColumnBuilder("col_b")
+                    .DataType("int")
+                    .GeneratedAlwaysAs("1980")
+                    .Nullable(false)
+                    .Build()
+            )
+            .Execute();
+        
+        newDeltaTable.ToDF().Show();
+        
+        var sourceDataFrame = spark.CreateDataFrame((new List<(int, long)>()
+        {
+            (1, 1980), (2, 1980), (3, 1980), (4, 1980), (5, 1980), (6, 1980)
+        }).Cast<ITuple>(), new StructType((List<StructField>) [new StructField("this_is_cola", new IntegerType(), false), new StructField("colb", new BigIntType(), false)])).Alias("source");
+
+        sourceDataFrame.Show();
+        newDeltaTable.As("target")
+            .Merge(sourceDataFrame, "source.this_is_cola = target.col_a")
+            .WhenMatchedUpdate(
+                (Col("target.col_a"), Col("source.this_is_cola")), 
+                (Col("target.col_b"), Col("source.colb")))   
+            .WhenNotMatchedInsert(
+                (Col("target.col_a"), Col("source.this_is_cola")), 
+                (Col("target.col_b"), Col("source.colb")))
+            .WithSchemaEvolution()
+            .Execute(spark);
+        
+        newDeltaTable.ToDF().Show();
+        
+        newDeltaTable.Optimize().ExecuteCompaction();
+        
+        newDeltaTable.Vacuum();
+    }
+}
--- src/example/delta-lake-example/Program.cs
+++ src/example/delta-lake-example/Program.cs
@@ -1,10 +1,5 @@
-﻿using Apache.Arrow;
-using Google.Protobuf.WellKnownTypes;
+﻿using delta_lake_example;
 using Spark.Connect.Dotnet.Sql;
-using Delta.Connect;
-using Spark.Connect;
-using Spark.Connect.Dotnet.Grpc;
-using static Spark.Connect.Dotnet.Sql.Functions;
 
 var spark = SparkSession
     .Builder
@@ -17,112 +12,6 @@ const string csvPath = "/tmp/delta/lake-csv";
 spark.Range(100).Write().Mode("overwrite").Format("delta").Write(deltaPath);
 spark.Range(100).Write().Mode("overwrite").Format("csv").Write(csvPath);
 
-bool IsDeltaTable(DeltaTable deltaTable)
-{
-    if (deltaTable.HasTableOrViewName)
-    {
-        throw new InvalidOperationException("The delta table must have a path and not a table name");
-    }
-    
-    var deltaRelation = new DeltaRelation()
-    {
-        IsDeltaTable = new IsDeltaTable()
-        {
-            Path = deltaTable.Path.Path_
-        }
-    };
 
-    var relation = new Relation
-    {
-        Extension = Any.Pack(deltaRelation)
-    };
-
-    var plan = new Plan()
-    {
-        Root = relation
-    };
-
-    var requestExecutor = new RequestExecutor(spark, plan, ArrowHandling.ArrowBuffers);
-    requestExecutor.Exec();
-
-    var recordBatches = requestExecutor.GetArrowBatches();
-        
-    var boolArray = recordBatches.First().Column("value") as BooleanArray;
-    var result = boolArray!.GetValue(0);
-    return result!.Value;
-}
-
-void UpdateDeltaTable(DeltaTable deltaTable, IList<Assignment> assignments, Expression condition)
-{
-    var deltaScan = new DeltaRelation()
-    {
-        Scan = new Scan()
-        {
-            Table = deltaTable
-        }
-    };
-        
-    var updateRelation = new DeltaRelation
-    {
-        UpdateTable = new UpdateTable()
-        {
-            Assignments =
-            {
-                assignments
-            }, 
-            Condition = condition,
-            Target = new Relation()
-            {
-                Extension = Any.Pack(deltaScan)
-            }
-        }
-    };
-    
-    var plan = new Plan()
-    {
-        Root = new Relation()
-        {
-            Extension = Any.Pack(updateRelation)
-        }
-    };
-    
-    var requestExecutor = new RequestExecutor(spark, plan, ArrowHandling.ArrowBuffers);
-    requestExecutor.Exec();
-    var recordBatches = requestExecutor.GetArrowBatches();
-    var longArray = (recordBatches.First().Column("num_affected_rows") as Int64Array)!;
-    
-    Console.WriteLine($"Update affected {longArray.GetValue(0)!.Value} rows");
-}
-
-
-var deltaTable = new DeltaTable()
-{
-    Path = new DeltaTable.Types.Path()
-    {
-        Path_ = deltaPath
-    }
-};
-
-var csvTable = new DeltaTable()
-{
-    Path = new DeltaTable.Types.Path()
-    {
-        Path_ = csvPath
-    }
-};
-
-Console.WriteLine($"The path '{deltaPath}' IsDeltaTable = {IsDeltaTable(deltaTable)}");
-Console.WriteLine($"The path '{csvPath}' IsDeltaTable = {IsDeltaTable(csvTable)}");
-
-UpdateDeltaTable(deltaTable,                //UPDATE TABLE
-        new List<Assignment>()                           
-                    {                                               
-                        new()                                      
-                        {                                          //
-                            Field = Col("id").Expression,    // SET id = 999
-                            Value = Lit(999).Expression            //
-                        }                                          
-                    },                                                  
-              Col("id").Le(50).Expression); // WHERE id <= 50
-
-
+// new ProtoExample().Run(spark, deltaPath, csvPath);
+new APIExample().Run(spark, deltaPath, csvPath);
